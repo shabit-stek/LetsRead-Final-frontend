fd379fac0fe298082fc99044f4a49120a96bc3fc
shabit-stek <shaad.ali@bitstek.io>
2026-03-04 18:05:50 +0530

    update

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 
-const API_URL = "https://letsread-final.onrender.com/api";
+const API_URL = "https://letsread-final-backend.onrender.com/api";
 
 // =====================
 // AUTH
